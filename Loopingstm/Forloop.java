388501ebd5610191fdd3d3407a83c552e0863159
Anam Harika <113250434+anamharika@users.noreply.github.com>
2024-03-14 19:26:27 +0530

    Update Forloop.java

--- Loopingstm/Forloop.java
+++ Loopingstm/Forloop.java
@@ -4,6 +4,7 @@ public class Forloop {
 	public static void main(String[] args) {
 		for(int i=1;i<=10;i++) {
 			System.out.println(i);
+			System.out.println("--------------------*------");
 		}
 	}
 
